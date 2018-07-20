481bb10d310b012bc770495c9ab7e3c4b89fd540
Damian Zabawa <damian.zabawa@ez.no>
2018-07-20 14:47:28 +0200

    EZP-27826: Warning on unmaintained/future versions
    https://jira.ez.no/browse/EZP-27826

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -3,36 +3,6 @@ $(document).ready(function () {
         $('.rst-other-versions').toggle();
     });
 
-    // remove elements, leave only 'versions'
-    var update = setInterval(function() {
-        if ($('.rst-other-versions .injected').length) {
-            clearInterval(update);
-            $('.rst-current-version span:first').html(' Change version');
-            $('.rst-other-versions .injected').html($('.rst-other-versions .injected dl:first').clone());
-
-            //replace url in version switcher
-            var resourceUrl = document.location.href.replace(
-                $('.rst-other-versions .injected strong dd a').attr('href'),
-                ''
-            );
-
-            $('.rst-other-versions .injected dd a').each( function() {
-                $(this).attr('href', $(this).attr('href') + resourceUrl);
-            });
-        }
-    }, 300);
-
-    $('img').each(function() {
-        if ($(this).attr('title')) {
-            $(this).wrap( "<figure></figure>" );
-            $(this).after( "<figcaption>" + $(this).attr('title') + "</figcaption>" );
-        }
-    });
-
-    $('.md-content a:not(.md-icon):not(.md-source)').filter(function() {
-        return this.hostname && this.hostname !== location.hostname;
-    }).addClass("external");
-
     // replace edit url
     var branchName = 'master',
         branchNameRegexp = /\/en\/([a-z0-9-_.]*)\//g.exec(document.location.href);
@@ -48,6 +18,67 @@ $(document).ready(function () {
     if (!/^\d+\.\d+$/.test(branchName) && branchName !== 'latest') {
         branchName = 'master';
     }
+
+    if (typeof window.doc_version_warning !== 'undefined') {
+        var doc_version_warning = window.doc_version_warning,
+            warningMessage = '';
+
+        if ($.inArray(branchName, doc_version_warning.previous_lts) !== -1) {
+            warningMessage = 'You are viewing documentation for an older LTS release. The latest LTS release is <span class="version">' + doc_version_warning.lts[0] + '</span>.';
+        }
+
+        if ($.inArray(branchName, doc_version_warning.ft) !== -1) {
+            warningMessage = 'You are viewing documentation for an older FT release. The latest release is <span class="version">' + doc_version_warning.latest[0] + '</span>.';
+        }
+
+        if ($.inArray(branchName, doc_version_warning.dev) !== -1) {
+            warningMessage = 'You are viewing documentation for a development version. The latest stable release is <span class="version">' + doc_version_warning.latest[0] + '</span>.';
+        }
+
+        if (warningMessage) {
+            $("article").prepend($(
+                '<div class="md-typeset admonition caution version-warning"> ' +
+                '<p class="admonition-title">Outdated version</p> ' +
+                '<p> ' + warningMessage + '</p>' +
+                '</div>'
+            ));
+        }
+    }
+
+    // remove elements, leave only 'versions'
+    var update = setInterval(function() {
+        if ($('.rst-other-versions .injected').length) {
+            clearInterval(update);
+            $('.rst-current-version span:first').html(' Change version');
+            $('.rst-other-versions .injected').html($('.rst-other-versions .injected dl:first').clone());
+
+            //replace url in version switcher
+            var currentVersion = $('.rst-other-versions .injected strong dd a').attr('href'),
+                resourceUrl = document.location.href.replace(currentVersion, '');
+
+            $('.rst-other-versions .injected dd a').each( function() {
+                $(this).attr('href', $(this).attr('href') + resourceUrl);
+            });
+
+            if ($('.version-warning').length) {
+                var version = $('.version-warning .version').html(),
+                    url = $('.rst-other-versions .injected dd a').first().attr('href').replace('latest', version);
+
+                $('.version-warning .version').html($('<a href ="' + url + '" class="external">' + version + '</a>'));
+            }
+        }
+    }, 300);
+
+    $('img').each(function() {
+        if ($(this).attr('title')) {
+            $(this).wrap( "<figure></figure>" );
+            $(this).after( "<figcaption>" + $(this).attr('title') + "</figcaption>" );
+        }
+    });
+
+    $('.md-content a:not(.md-icon):not(.md-source)').filter(function() {
+        return this.hostname && this.hostname !== location.hostname;
+    }).addClass("external");
 
     docsearch({
         apiKey: 'bfb5bd7cad971d31ef8be599174334f3',
