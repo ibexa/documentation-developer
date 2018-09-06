bf856375ba664d095fab792a2c978233ee9ded17
DominikaK <dominika.kurek@ez.no>
2018-09-06 11:58:04 +0200

    Prevent image link enlarging (an ugly way)

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -103,7 +103,7 @@ $(document).ready(function () {
     });
 
     // Image enlargement modal
-    $("img").click( function(){
+    $("img:not(.card-img-top)").click( function(){
         $('#enlargedImage').attr('src', $(this).attr('src'));
         if ($(this).attr('title')) {
             $('#modalCaption').html($(this).attr('title'));
