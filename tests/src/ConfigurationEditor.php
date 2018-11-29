a17b70e23f8e485ae869a05281f5b707f41f7639
Marek Nocoń <mnocon@users.noreply.github.com>
2018-11-29 13:26:49 +0100

    Automated code testing tool for documentation (#467)

---
+++ tests/src/ConfigurationEditor.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\DeveloperDocumentation\Test;
+
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\Yaml\Yaml;
+
+class ConfigurationEditor
+{
+    /** @var string */
+    private $configFilePath;
+
+    public function __construct(string $configFilePath)
+    {
+        $this->configFilePath = $configFilePath;
+    }
+
+    public function add($configEntries): void
+    {
+        $parsedConfig = Yaml::parse(file_get_contents($this->configFilePath));
+
+        foreach ($configEntries as $key => $value) {
+            $this->addSingle($parsedConfig, $key, $value);
+        }
+
+        file_put_contents($this->configFilePath, Yaml::dump($parsedConfig, 5, 4));
+    }
+
+    private function addSingle(&$parsedConfig, $key, $value): void
+    {
+        $propertyAccessor = PropertyAccess::createPropertyAccessor();
+        $parsedKey = $this->parseKey($key);
+        $currentValue = $propertyAccessor->getValue($parsedConfig, $parsedKey);
+
+        if (is_array($currentValue)) {
+            if (is_array($value)) {
+                $value = array_merge($currentValue, $value);
+            } else {
+                $currentValue[] = $value;
+                $value = $currentValue;
+            }
+        }
+
+        $propertyAccessor->setValue($parsedConfig, $parsedKey, $value);
+    }
+
+    private function parseKey(string $key): string
+    {
+        $keys = explode('.', $key);
+        $parsed = '';
+        foreach ($keys as $keyPart) {
+            $parsed .= sprintf('[%s]', $keyPart);
+        }
+
+        return $parsed;
+    }
+}
---
+++ tests/src/Context/ContentContext.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\DeveloperDocumentation\Test\Context;
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\TableNode;
+use eZ\Publish\API\Repository\ContentService;
+use eZ\Publish\API\Repository\LocationService;
+use eZ\Publish\API\Repository\Repository;
+use EzSystems\DeveloperDocumentation\Test\ContentDataCreator;
+
+class ContentContext implements Context
+{
+    /** @var Repository */
+    private $repository;
+
+    /** @var ContentService */
+    private $contentService;
+
+    /** @var LocationService */
+    private $locationService;
+
+    private const ADMIN_USER_ID = 14;
+
+    private const EZ_PLATFORM_FOLDER_CONTENT_ID = 1;
+
+    private $pathStringLocationMap;
+
+    private $basePath;
+
+    public function __construct($repository, $basePath)
+    {
+        $this->basePath = $basePath;
+        $this->repository = $repository;
+        $this->contentService = $this->repository->getContentService();
+        $this->locationService = $this->repository->getLocationService();
+        $this->pathStringLocationMap = ['Home' => 2];
+    }
+
+    public function createContent($contentTypeIdentifier, $parentPathString, $contentItemData): void
+    {
+        $permissionResolver = $this->repository->getPermissionResolver();
+        $user = $this->repository->getUserService()->loadUser(self::ADMIN_USER_ID);
+        $permissionResolver->setCurrentUserReference($user);
+
+        $locationCreateStruct = $this->locationService->newLocationCreateStruct($this->pathStringLocationMap[$parentPathString]);
+        $contentType = $this->repository->getContentTypeService()->loadContentTypeByIdentifier($contentTypeIdentifier);
+        $contentCreateStruct = $this->contentService->newContentCreateStruct($contentType, 'eng-GB');
+
+        $contentDataCreator = new ContentDataCreator($this->basePath);
+        $contentDataCreator->fillWithData($contentCreateStruct, $contentTypeIdentifier, $contentItemData);
+
+        $draft = $this->contentService->createContent($contentCreateStruct, [$locationCreateStruct]);
+        $content = $this->contentService->publishVersion($draft->versionInfo);
+
+        $newItemPathString = sprintf('%s/%s', $parentPathString, $contentItemData['contentName']);
+        $this->pathStringLocationMap[$newItemPathString] = $content->contentInfo->mainLocationId;
+    }
+
+    /**
+     * @Given I delete eZ Platform Folder under Home
+     */
+    public function deleteContentItem(): void
+    {
+        $permissionResolver = $this->repository->getPermissionResolver();
+        $user = $this->repository->getUserService()->loadUser(self::ADMIN_USER_ID);
+        $permissionResolver->setCurrentUserReference($user);
+
+        $contentInfo = $this->contentService->loadContentInfo(self::EZ_PLATFORM_FOLDER_CONTENT_ID);
+        $this->contentService->deleteContent($contentInfo);
+    }
+
+    /**
+     * @Given I create :contentTypeIdentifier Content items in :parentPathString
+     */
+    public function createContentItems($contentTypeIdentifier, $parentPathString, TableNode $contentItemsData): void
+    {
+        foreach ($contentItemsData as $contentItemData) {
+            $this->createContent($contentTypeIdentifier, $parentPathString, $contentItemData);
+        }
+    }
+}
