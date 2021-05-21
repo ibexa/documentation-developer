8f7a869543eda31074dc6836c5d9950b331028ce
Tomasz Dąbrowski <64841871+dabrt@users.noreply.github.com>
2021-05-21 13:04:02 +0200

    IBX-323 - Extending Solr (#1369)
    * IBX-323 - Extending Solr
    
    Co-Authored-By: DominikaK <dominika.kurek@ez.no>
    
    * Revert changes to package structure
    
    Co-Authored-By: DominikaK <dominika.kurek@ez.no>
    
    * Code changes
    
    Co-Authored-By: DominikaK <dominika.kurek@ez.no>
    
    * Added link to solr aggregations
    
    Co-Authored-By: DominikaK <dominika.kurek@ez.no>
    
    * Code review comments implemented
    
    * Apply suggestions from code review
    
    Co-authored-by: DominikaK <dominika.kurek@ez.no>
    
    * Move code to code_samples
    
    * Replace code with code includes
    
    * Update docs/guide/search/extend_solr.md
    
    * Code include fixes
    
    * Formatting fix
    
    Co-authored-by: DominikaK <dominika.kurek@ez.no>

---
+++ code_samples/search/solr/src/Query/Aggregation/PriorityRangeAggregation.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Query\Aggregation;
+
+use eZ\Publish\API\Repository\Values\Content\Query\Aggregation\AbstractRangeAggregation;
+use eZ\Publish\API\Repository\Values\Content\Query\Aggregation\LocationAggregation;
+
+final class PriorityRangeAggregation extends AbstractRangeAggregation implements LocationAggregation
+{
+    ...
+}
