b970c32ae53f99f402126cb3eb61fadc7f8e88f0
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2022-04-19 17:10:19 +0200

    Remove ¶ while displaying search results (#1590)
    (cherry picked from commit 23f5bf256438c0129c942a70c0a8d0a3a787e7d9)

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -83,6 +83,19 @@ $(document).ready(function() {
         apiKey: 'bfb5bd7cad971d31ef8be599174334f3',
         indexName: 'ezplatform',
         inputSelector: '#search_input',
+        transformData: function(hits) {
+            let removedPattern = '¶';
+            $.each(hits, function(index, hit) {
+                for (let lvl=2; lvl<=6; lvl++) {
+                    if (null !== hit.hierarchy['lvl'+lvl]) {
+                        hits[index].hierarchy['lvl' + lvl] = hit.hierarchy['lvl' + lvl].replace(removedPattern, '');
+                    }
+                    if ('undefined' !== typeof hit._highlightResult.hierarchy['lvl'+lvl]) {
+                        hits[index]._highlightResult.hierarchy['lvl'+lvl].value = hit._highlightResult.hierarchy['lvl'+lvl].value.replace(removedPattern, '');
+                    }
+                }
+            });
+        },
         algoliaOptions: {
             facetFilters: ['lang:en', 'version:' + branchName],
             hitsPerPage: 10,
