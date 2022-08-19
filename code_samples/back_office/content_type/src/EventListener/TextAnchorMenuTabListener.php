a671335771b4c3d3f842c7b2cb8723a3be739405
Justyna Koralewicz <79849375+juskora@users.noreply.github.com>
2022-08-19 08:27:36 +0200

    Update code_samples/back_office/content_type/src/EventListener/TextAnchorMenuTabListener.php
    Co-authored-by: Maciej Kobus <webhdx@users.noreply.github.com>

--- code_samples/back_office/content_type/src/EventListener/TextAnchorMenuTabListener.php
+++ code_samples/back_office/content_type/src/EventListener/TextAnchorMenuTabListener.php
@@ -1,24 +1,33 @@
 <?php
+
 namespace App\EventListener;
+
 use Ibexa\AdminUi\Menu\ContentEditAnchorMenuBuilder;
 use Ibexa\AdminUi\Menu\Event\ConfigureMenuEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
 class TextAnchorMenuTabListener implements EventSubscriberInterface
 {
     public static function getSubscribedEvents(): array
     {
         return [ConfigureMenuEvent::CONTENT_EDIT_ANCHOR_MENU => 'onAnchorMenuConfigure'];
     }
+    
     public function onAnchorMenuConfigure(ConfigureMenuEvent $event): void
     {
+        // access anchor menu root item
         $menu = $event->getMenu();
 
+        // if you need to access "Content" tab, use ITEM__CONTENT constant:
         $menu[ContentEditAnchorMenuBuilder::ITEM__CONTENT];
 
+        // if you need to access "Meta" tab, use ITEM__META constant:
         $menu[ContentEditAnchorMenuBuilder::ITEM__META];
 
-        $menu->addChild('item', ['attributes' => ['data-target-id' => 'ibexa-edit-content-sections-item']]);
+        // Adding new tab called "New tab"
+        $menu->addChild('New tab', ['attributes' => ['data-target-id' => 'ibexa-edit-content-sections-new-tab']]);
 
-        $menu['item']->addChild('item_2', ['attributes' => ['data-target-id' => 'ibexa-edit-content-sections-item-item_2']]);
+        // Add second level item "2nd level item" to previously created "New tab" tab
+        $menu['New tab']->addChild('2nd level item', ['attributes' => ['data-target-id' => 'ibexa-edit-content-sections-new-tab-item_2']]);
     }
 }
