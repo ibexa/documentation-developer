e16beaa1775d84084e3073e8201ba96c4e33d024
Slawomir Dolzycki-Uchto <Nattfarinn@users.noreply.github.com>
2022-09-05 14:29:50 +0200

    IBX-3006: Macro plugin for adding page cards (#1658)

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import os
+import pprint
+import re
+from mkdocs.structure.pages import Page
+from mkdocs.utils import meta
+
+CARDS_MARKDOWN_TEMPLATE = """
+-  [%s](%s)
+{.title}
+
+    %s
+{.description}
+"""
+
 
 def define_env(env):
     """
@@ -23,3 +36,45 @@ def define_env(env):
             lines = f.readlines()
         line_range = lines[start_line:end_line]
         return glue.join(line_range)
+
+    @env.macro
+    def cards(pages, columns=1):
+        current_page = env.variables.page
+        absolute_url = current_page.abs_url
+        url_parts = re.search("^/([^/]+)/([^/]+)/", absolute_url)
+        (language, version) = url_parts.groups()
+
+        if isinstance(pages, str):
+            pages = [pages]
+        cards_markdown = []
+        for page in pages:
+            with open("docs/%s.md" % page, "r") as doc_file:
+                doc = doc_file.read()
+                match = re.search("^# (.*)", doc, re.MULTILINE)
+                if match:
+                    header = match.groups()[0]
+                else:
+                    header = ""
+                default_meta = {
+                    "title": header,
+                    "short": "",
+                    "description": ""
+                }
+                doc_meta = {
+                    **default_meta,
+                    **meta.get_data(doc)[1]
+                }
+                cards_markdown.append(
+                    CARDS_MARKDOWN_TEMPLATE % (
+                        doc_meta['short'] or doc_meta['title'],
+                        '/'.join((
+                            '',
+                            language,
+                            version,
+                            page
+                        )),
+                        doc_meta['description'] or "&nbsp;"
+                    )
+                )
+
+        return """<div class="cards col-%s" markdown>%s</div>""" % (columns, "\n".join(cards_markdown))
