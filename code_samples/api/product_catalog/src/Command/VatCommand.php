092cff603e6b5cebd979dc6d976bccbca391d46c
DominikaK <dominika.kurek@ez.no>
2022-10-27 08:46:36 +0200

    IBX-3582: Extend catalog-related PHP API doc (#1784)

---
+++ code_samples/api/product_catalog/src/Command/VatCommand.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types=1);
+
+namespace App\Command;
+
+use Ibexa\Contracts\Core\Repository\PermissionResolver;
+use Ibexa\Contracts\Core\Repository\UserService;
+use Ibexa\Contracts\ProductCatalog\RegionServiceInterface;
+use Ibexa\Contracts\ProductCatalog\VatServiceInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class VatCommand extends Command
+{
+    private UserService $userService;
+
+    private PermissionResolver $permissionResolver;
+
+    private VatServiceInterface $vatService;
+
+    private RegionServiceInterface $regionService;
+
+    public function __construct(
+        UserService $userService,
+        PermissionResolver $permissionResolver,
+        VatServiceInterface $vatService,
+        RegionServiceInterface $regionService
+    ) {
+        $this->userService = $userService;
+        $this->permissionResolver = $permissionResolver;
+        $this->vatService = $vatService;
+        $this->regionService = $regionService;
+        parent::__construct('doc:vat');
+    }
+
+    public function configure(): void
+    {
+        $this
+            ->setDefinition([
+                new InputArgument('productCode', InputArgument::REQUIRED, 'Product code'),
+            ]);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $user = $this->userService->loadUserByLogin('admin');
+        $this->permissionResolver->setCurrentUserReference($user);
+
+        $region = $this->regionService->getRegion('poland');
+
+        $vatCategories = $this->vatService->getVatCategories($region);
+
+        foreach ($vatCategories as $category) {
+            $output->writeln($category->getIdentifier() . ': ' . $category->getVatValue());
+        }
+
+        $vatCategory = $this->vatService->getVatCategoryByIdentifier($region, 'reduced');
+
+        $output->writeln($vatCategory->getVatValue());
+
+        return self::SUCCESS;
+    }
+}
