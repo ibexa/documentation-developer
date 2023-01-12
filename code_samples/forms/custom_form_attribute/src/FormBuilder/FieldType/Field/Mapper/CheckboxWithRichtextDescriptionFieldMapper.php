81cb7c7529ce94833d69e3b17defbb13276bc0fc
julitafalcondusza <julita.dusza@ibexa.co>
2023-01-12 15:41:18 +0100

    Fixes added after tests

---
+++ code_samples/forms/custom_form_attribute/src/FormBuilder/FieldType/Field/Mapper/CheckboxWithRichtextDescriptionFieldMapper.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\FormBuilder\FieldType\Field\Mapper;
+
+use EzSystems\EzPlatformFormBuilder\FieldType\Field\Mapper\GenericFieldMapper;
+use EzSystems\EzPlatformFormBuilder\FieldType\Model\Field;
+
+class CheckboxWithRichtextDescriptionFieldMapper extends GenericFieldMapper
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapFormOptions(Field $field, array $constraints): array
+    {
+        $options = parent::mapFormOptions($field, $constraints);
+        $options['label'] = $field->getAttributeValue('label');
+        $options['richtext_description'] = $field->getAttributeValue('richtext_description');
+
+        return $options;
+    }
+}
