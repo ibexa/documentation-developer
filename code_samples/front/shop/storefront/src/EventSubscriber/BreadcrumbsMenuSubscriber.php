e058f919e08b481b3a19fb92701e405af14142fd
Justyna Koralewicz <79849375+juskora@users.noreply.github.com>
2023-02-02 10:08:19 +0100

    IBX-4642: Storefront config (#1875)

---
+++ code_samples/front/shop/storefront/src/EventSubscriber/BreadcrumbsMenuSubscriber.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use Ibexa\Bundle\Storefront\Menu\Builder\BreadcrumbsMenuBuilder;
+use Ibexa\Contracts\Storefront\Menu\Event\ConfigureMenuEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class BreadcrumbsMenuSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            BreadcrumbsMenuBuilder::PRODUCT_MENU => ['onBreadcrumbsMenuConfigure', 0],
+        ];
+    }
+
+    public function onBreadcrumbsMenuConfigure(ConfigureMenuEvent $event)
+    {
+        $menu = $event->getMenu();
+
+        // Replace link to home with link to product catalog root
+        $menu->getChild('location_2')->setUri('/product-catalog');
+    }
+}
