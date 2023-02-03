e2bbfebe88220ab6447200a8f8ce3f688893ad83
julitafalcondusza <117284672+julitafalcondusza@users.noreply.github.com>
2023-02-03 14:49:41 +0100

    IBX-3133: Create Form Builder form attribute - v4.3 (#1842)
    * Procedure how to create Form Builder Form attribute added
    
    * Structure and procedure from 3.3 added - to be updated for 4.3
    
    * Content & paths corrections
    
    * Codes update
    
    * Folder with code deleted
    
    * Snippets updated to 4.3
    
    * Fixes after CS-fixer
    
    * Delete .php-cs-fixer.cache
    
    * Delete composer.lock
    
    * Delete form_attribute.yaml
    
    * Added to mkdocs
    
    * Corrections after review
    
    * File rename & code sample updated
    
    ---------
    
    Co-authored-by: Nattfarinn <nattfarinn@gmail.com>

---
+++ code_samples/forms/custom_form_attribute/src/FormBuilder/Form/Type/FieldAttribute/AttributeRichtextDescriptionType.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace App\FormBuilder\Form\Type\FieldAttribute;
+
+use Ibexa\FieldTypeRichText\Form\Type\RichTextType;
+use Symfony\Component\Form\AbstractType;
+
+class AttributeRichtextDescriptionType extends AbstractType
+{
+    /**
+     * @return string|null
+     */
+    public function getParent()
+    {
+        return RichTextType::class;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBlockPrefix()
+    {
+        return 'field_configuration_attribute_richtext';
+    }
+}
---
+++ code_samples/forms/custom_form_attribute/src/FormBuilder/FormSubmission/Converter/RichtextDescriptionFieldSubmissionConverter.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace App\FormBuilder\FormSubmission\Converter;
+
+use Ibexa\FormBuilder\FormSubmission\Converter\BooleanFieldSubmissionConverter;
+
+class RichtextDescriptionFieldSubmissionConverter extends BooleanFieldSubmissionConverter
+{
+}
