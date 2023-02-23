5b2d57a020810dd79dec337787007351795cb234
Adrien Dupuis <adrien.dupuis@ibexa.co>
2023-02-23 09:42:02 +0100

    Add a script to generate a PHP API reference

---
+++ tools/php_api_ref/phpdoc.sh
@@ -0,0 +1,24 @@
+AUTH_JSON=$1;
+OUTPUT_DIR=$2;
+
+FLAVOR=commerce;
+VERSION='4.4.*';
+TMP_DXP_DIR=/tmp/ibexa-dxp-phpdoc;
+if [ -z "$OUPUT_DIR" ]; then
+  OUTPUT_DIR="$(pwd)/docs/api/php_api/php_api_reference";
+fi;
+
+rm -rf $TMP_DXP_DIR;
+mkdir $TMP_DXP_DIR;
+cd $TMP_DXP_DIR;
+
+composer create-project ibexa/$FLAVOR-skeleton:$VERSION . --no-interaction --no-install --ignore-platform-reqs --no-scripts;
+if [ -n "$AUTH_JSON" ]; then
+  cp $AUTH_JSON ./;
+fi;
+composer install --no-interaction --ignore-platform-reqs --no-scripts;
+
+curl -LO "https://github.com/phpDocumentor/phpDocumentor/releases/download/v3.3.1/phpDocumentor.phar";
+php phpDocumentor.phar -t $OUTPUT_DIR -d ./vendor/ibexa/ -d ./vendor/ezsystems/ -d ./vendor/silversolutions/ -i tests/;
+
+rm -rf $TMP_DXP_DIR;
