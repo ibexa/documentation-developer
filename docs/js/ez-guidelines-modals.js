3aa572a0104c58b4e12db15afb040da8e65ddc0d
DominikaK <dominika.kurek@ez.no>
2023-03-09 09:12:35 +0100

    Update doc site dependencies (#1917)

--- docs/js/ez-guidelines-modals.js
+++
@@ -1,7 +0,0 @@
-$('.btn-modal-launcher').on("click", function (e) {
-    $('div').attr('data-backdrop', 'true');
-    
-    $('#trash-location-modal').addClass('modal-backdrop');
-    $('#view-notifications').addClass('modal-backdrop');
-    $('#ez-modal--custom-url-alias').addClass('modal-backdrop');
-});
