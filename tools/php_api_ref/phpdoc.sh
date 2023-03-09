060038bcd570a9fed9a16ff815111df2727f1e73
Adrien Dupuis <adrien.dupuis@ibexa.co>
2023-03-09 15:26:47 +0100

    PhpDoc config

--- tools/php_api_ref/phpdoc.sh
+++ tools/php_api_ref/phpdoc.sh
@@ -4,7 +4,8 @@ OUTPUT_DIR=$2;
 FLAVOR='commerce';
 VERSION='4.4.*';
 TMP_DXP_DIR=/tmp/ibexa-dxp-phpdoc;
-PHPDOC_CONF="$(pwd)/tools/php_api_ref/.phpdoc";
+PHPDOC_CONF="$(pwd)/tools/php_api_ref/phpdoc.dist.xml";
+PHPDOC_DIR="$(pwd)/tools/php_api_ref/.phpdoc";
 if [ -z "$OUPUT_DIR" ]; then
   OUTPUT_DIR="$(pwd)/docs/api/php_api/php_api_reference";
 fi;
@@ -18,7 +19,8 @@ if [ -n "$AUTH_JSON" ]; then
   cp $AUTH_JSON ./;
 fi;
 composer install --no-interaction --ignore-platform-reqs --no-scripts;
-cp -R $PHPDOC_CONF ./;
+cp $PHPDOC_CONF ./;
+cp -R $PHPDOC_DIR ./;
 curl -LO "https://github.com/phpDocumentor/phpDocumentor/releases/download/v3.3.1/phpDocumentor.phar";
 php phpDocumentor.phar -t $OUTPUT_DIR -d ./vendor/ibexa/ -d ./vendor/ezsystems/ -d ./vendor/silversolutions/ -i tests/;
 
