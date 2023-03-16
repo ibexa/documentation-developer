48795aeb1c5a00bd3a76db17da48686467b53b8f
Adrien Dupuis <adrien.dupuis@ibexa.co>
2023-03-16 16:21:39 +0100

    phpdoc.sh: Move to output dir after "indices/files" removal
    Avoid `mkdocs serve` to crash during removal with such error:
    `FileNotFoundError: [Errno 2] No such file or directory: 'docs/api/php_api/php_api_reference/files/vendor-ibexa-admin-ui-assets-src-bundle-ibexaadminuiassetsbundle.html'`

--- tools/php_api_ref/phpdoc.sh
+++ tools/php_api_ref/phpdoc.sh
@@ -8,7 +8,7 @@ VERSION='4.4.*';
 TMP_DXP_DIR=/tmp/ibexa-dxp-phpdoc;
 PHPDOC_CONF="$(pwd)/tools/php_api_ref/phpdoc.dist.xml";
 PHPDOC_DIR="$(pwd)/tools/php_api_ref/.phpdoc";
-if [ -z "$OUPUT_DIR" ]; then
+if [ -z "$OUTPUT_DIR" ]; then
   OUTPUT_DIR="$(pwd)/docs/api/php_api/php_api_reference";
 fi;
 
@@ -25,7 +25,8 @@ composer install --no-interaction --ignore-platform-reqs --no-scripts;
 cp $PHPDOC_CONF ./;
 cp -R $PHPDOC_DIR ./;
 curl -LO "https://github.com/phpDocumentor/phpDocumentor/releases/download/v3.3.1/phpDocumentor.phar";
-php phpDocumentor.phar -t $OUTPUT_DIR;
-rm -rf $OUTPUT_DIR/files $OUTPUT_DIR/indices
+php phpDocumentor.phar -t php_api_reference;
+rm -rf php_api_reference/files php_api_reference/indices;
+cp -rf php_api_reference/* $OUTPUT_DIR;
 
 rm -rf $TMP_DXP_DIR;
