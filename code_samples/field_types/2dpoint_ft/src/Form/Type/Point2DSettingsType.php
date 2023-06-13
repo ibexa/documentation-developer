48a43d3261da7b3f58fdfe52d19df9be3ce027fe
ibexa-yuna <ibexa-yuna@users.noreply.github.com>
2023-06-13 07:42:32 +0000

    PHP CS Fixes

--- code_samples/field_types/2dpoint_ft/src/Form/Type/Point2DSettingsType.php
+++ code_samples/field_types/2dpoint_ft/src/Form/Type/Point2DSettingsType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 final class Point2DSettingsType extends AbstractType
-
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
--- code_samples/field_types/2dpoint_ft/steps/step_3/Type.php
+++ code_samples/field_types/2dpoint_ft/steps/step_3/Type.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 namespace App\FieldType\Point2D;
 
 use App\Form\Type\Point2DType;
-use Ibexa\Contracts\Core\FieldType\Generic\Type as GenericType;
 use Ibexa\Contracts\ContentForms\Data\Content\FieldData;
 use Ibexa\Contracts\ContentForms\FieldType\FieldValueFormMapperInterface;
+use Ibexa\Contracts\Core\FieldType\Generic\Type as GenericType;
 use Symfony\Component\Form\FormInterface;
 
 final class Type extends GenericType implements FieldValueFormMapperInterface
@@ -15,13 +15,13 @@ final class Type extends GenericType implements FieldValueFormMapperInterface
     {
         return 'point2d';
     }
-    
+
     public function mapFieldValueForm(FormInterface $fieldForm, FieldData $data)
     {
         $definition = $data->fieldDefinition;
         $fieldForm->add('value', Point2DType::class, [
             'required' => $definition->isRequired,
-            'label' => $definition->getName()
+            'label' => $definition->getName(),
         ]);
     }
 }
