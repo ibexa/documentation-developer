f846a4dfc26c340542f075ef5ee7e50aa699c9eb
Tomasz Dąbrowski <64841871+dabrt@users.noreply.github.com>
2023-06-29 10:50:18 +0200

    IBX-5609 - Extend Shipping module

---
+++ code_samples/front/shop/shipping/src/ShippingMethodType/Voter/CustomVoter.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ShippingMethodType\Voter;
+
+use Ibexa\Contracts\Cart\Value\CartInterface;
+use Ibexa\Contracts\Shipping\ShippingMethod\Voter\AbstractVoter;
+use Ibexa\Contracts\Shipping\Value\ShippingMethod\ShippingMethodInterface;
+
+final class CustomVoter extends AbstractVoter
+{
+    protected function getVote(ShippingMethodInterface $method, CartInterface $cart): bool
+    {
+        return $method->getOptions()->get('customer_identifier') === 'foo';
+    }
+}
