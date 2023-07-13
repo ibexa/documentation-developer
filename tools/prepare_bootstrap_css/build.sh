a85c0bfc5515621a2a335fea101878fc1c7a02ee
Adrien Dupuis <adrien.dupuis@ibexa.co>
2023-07-13 14:20:09 +0200

    remove outdated prepare_guidelines_css

--- tools/prepare_bootstrap_css/build.sh
+++
@@ -1,3 +0,0 @@
-curl 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' | sed 's/"Courier New",monospace/"Courier New",monospace;/' > bootstrap.min.css
-lessc prefix_bootstrap.less bootstrap-iso.css \
-  && mv -v bootstrap-iso.css ../../docs/css/ ;
