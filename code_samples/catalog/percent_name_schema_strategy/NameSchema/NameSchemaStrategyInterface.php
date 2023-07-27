ccf8168736ff068b5789927ce25e96a1f3b29238
julitafalcondusza <julita.dusza@ibexa.co>
2023-07-27 12:00:35 +0200

    Code fixes

--- code_samples/catalog/percent_name_schema_strategy/NameSchema/NameSchemaStrategyInterface.php
+++ code_samples/catalog/percent_name_schema_strategy/NameSchema/NameSchemaStrategyInterface.php
@@ -1,31 +1,20 @@
 <?php
 
-declare(strict_types=1);
-
-namespace App\Attribute\NameSchema;
+namespace App\Attribute\Percent;
 
 use Ibexa\Contracts\ProductCatalog\NameSchema\NameSchemaStrategyInterface;
 use Ibexa\Contracts\ProductCatalog\Values\AttributeDefinitionInterface;
 
 final class NameSchemaStrategyInterface implements NameSchemaStrategyInterface
 {
-    /**
-     * @param mixed $value
-     */
+
     public function resolve(AttributeDefinitionInterface $attributeDefinition, $value, string $languageCode): string
     {
-        if ($value instanceof CustomValueInterface) {
-            return $value->getValue();
-        }
-
-        return '';
+        return ($value * 100) . "%";
     }
 
-    /**
-     * @param mixed $value
-     */
     public function supports(AttributeDefinitionInterface $attributeDefinition, $value): bool
     {
-        return $attributeDefinition->getType()->getIdentifier() === 'custom_value';
+        return $attributeDefinition->getType()->getIdentifier() === 'percent';
     }
 }
