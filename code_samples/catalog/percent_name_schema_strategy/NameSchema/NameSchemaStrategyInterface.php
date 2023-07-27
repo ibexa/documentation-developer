36038caef53d3a9fe5d4a3ccda555c64f54e6abb
julitafalcondusza <julitafalcondusza@users.noreply.github.com>
2023-07-27 09:49:53 +0000

    PHP CS Fixes

--- code_samples/catalog/percent_name_schema_strategy/NameSchema/NameSchemaStrategyInterface.php
+++ code_samples/catalog/percent_name_schema_strategy/NameSchema/NameSchemaStrategyInterface.php
@@ -7,7 +7,7 @@ namespace App\Attribute\NameSchema;
 use Ibexa\Contracts\ProductCatalog\NameSchema\NameSchemaStrategyInterface;
 use Ibexa\Contracts\ProductCatalog\Values\AttributeDefinitionInterface;
 
-final class PercentNameSchemaStrategy implements NameSchemaStrategyInterface
+final class NameSchemaStrategyInterface implements NameSchemaStrategyInterface
 {
     /**
      * @param mixed $value
