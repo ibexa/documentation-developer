c6a9cbb0f35b0bd290797251ad2b06572abb147b
Adrien Dupuis <adrien.dupuis@ibexa.co>
2023-08-01 15:04:49 +0200

    phpdoc.sh: Get ready for 4.6.0

--- tools/php_api_ref/phpdoc.sh
+++ tools/php_api_ref/phpdoc.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+set +x;
+
 AUTH_JSON=$1;
 OUTPUT_DIR=$2;
 
 FLAVOR='commerce';
-VERSION='4.5.*';
+VERSION='4.6.0-beta1';
 TMP_DXP_DIR=/tmp/ibexa-dxp-phpdoc;
 PHPDOC_CONF="$(pwd)/tools/php_api_ref/phpdoc.dist.xml";
 PHPDOC_DIR="$(pwd)/tools/php_api_ref/.phpdoc";
@@ -44,7 +46,7 @@ fi;
 
 echo -n 'Building package→edition map… ';
 map=$PHPDOC_DIR/template/package-edition-map.twig;
-editions=(oss content experience commerce);
+editions=(oss headless experience commerce);
 if [[ -f $map ]]; then
   rm $map;
 fi;
