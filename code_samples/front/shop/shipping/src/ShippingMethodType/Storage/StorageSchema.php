1544518a6dc8da36324547735e11d14ec67ee2a3
Tomasz Dąbrowski <64841871+dabrt@users.noreply.github.com>
2023-08-04 10:14:59 +0200

    IBX-5609 - Extend Shipping module (#2037)
    * IBX-5609 - Extend Shipping module
    
    ---------
    
    Co-authored-by: dabrt <dabrt@users.noreply.github.com>
    Co-authored-by: Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
    Co-authored-by: Magdalena Dziadosz <35031941+MagdalenaZuba@users.noreply.github.com>

---
+++ code_samples/front/shop/shipping/src/ShippingMethodType/Storage/StorageSchema.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ShippingMethodType\Storage;
+
+use Ibexa\Shipping\Persistence\Legacy\ShippingMethod\AbstractOptionsStorageSchema;
+
+final class StorageSchema extends AbstractOptionsStorageSchema
+{
+    public const TABLE_NAME = 'ibexa_shipping_method_region_custom';
+    public const COLUMN_ID = 'id';
+    public const COLUMN_CUSTOMER_ID = 'customer_id';
+}
