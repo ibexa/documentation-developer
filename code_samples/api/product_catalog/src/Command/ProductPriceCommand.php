29818baee93ec7bb1a05bc819257f2ba96439f75
Tomasz Dąbrowski <64841871+dabrt@users.noreply.github.com>
2023-09-04 17:24:36 +0200

    Add Resolve prices step

--- code_samples/api/product_catalog/src/Command/ProductPriceCommand.php
+++ code_samples/api/product_catalog/src/Command/ProductPriceCommand.php
@@ -5,9 +5,11 @@ namespace App\Command;
 use Ibexa\Contracts\Core\Repository\PermissionResolver;
 use Ibexa\Contracts\Core\Repository\UserService;
 use Ibexa\Contracts\ProductCatalog\CurrencyServiceInterface;
+use Ibexa\Contracts\ProductCatalog\PriceResolverInterface;
 use Ibexa\Contracts\ProductCatalog\ProductPriceServiceInterface;
 use Ibexa\Contracts\ProductCatalog\ProductServiceInterface;
 use Ibexa\Contracts\ProductCatalog\Values\Price\Create\Struct\ProductPriceCreateStruct;
+use Ibexa\Contracts\ProductCatalog\Values\Price\PriceContext;
 use Ibexa\Contracts\ProductCatalog\Values\Price\PriceQuery;
 use Ibexa\Contracts\ProductCatalog\Values\Price\Query\Criterion\Currency;
 use Ibexa\Contracts\ProductCatalog\Values\Price\Query\Criterion\CustomerGroup;
@@ -21,6 +23,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class ProductPriceCommand extends Command
 {
     private ProductPriceServiceInterface $productPriceService;
+
+    private PriceResolverInterface $priceResolver;
 
     private ProductServiceInterface $productService;
 
@@ -86,16 +90,21 @@ final class ProductPriceCommand extends Command
             $output->writeln($price);
         }
 
-        $priceCriterions = [
+        $priceCriteria = [
             new Currency('USD'),
             new CustomerGroup('customer_group_1'),
             new Product('ergo_desk'),
         ];
-        
-        $priceQuery = new PriceQuery(new LogicalOr(...$priceCriterions));
+
+        $priceQuery = new PriceQuery(new LogicalOr(...$priceCriteria));
         $prices = $this->priceService->findPrices($priceQuery);
 
         $output->writeln(sprintf('Found %d prices with provided criteria', count($prices)));
+
+        $context = new PriceContext($currency);
+        $price = $this->priceResolver->resolvePrice($product, $context);
+
+        $output->writeln('Price in ' . $currency->getCode() . ' for ' . $product->getName() . ' is ' . $price);
 
         return self::SUCCESS;
     }
