88627b90122b3d09f9ca0d21fb4ffc777b96f591
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2023-09-06 16:01:33 +0200

    IBX-6409: Render in PHP (#2120)
    Co-authored-by: David Sayre <1364366+davidsayre@users.noreply.github.com>
    Co-authored-by: Mateusz Bieniek <mateusz@bieniek.dev>
    Co-authored-by: Magdalena Dziadosz <35031941+MagdalenaZuba@users.noreply.github.com>

---
+++ code_samples/front/render_content_in_php/src/Command/ViewCommand.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+
+namespace App\Command;
+
+use Ibexa\Core\MVC\Symfony\View\Builder\ContentViewBuilder;
+use Ibexa\Core\MVC\Symfony\View\Renderer\TemplateRenderer;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ViewCommand extends Command
+{
+    protected static $defaultName = 'app:view';
+
+    private ContentViewBuilder $contentViewBuilder;
+
+    private TemplateRenderer $templateRenderer;
+
+    private ResponseTagger $responseTagger;
+
+    public function __construct(
+        ContentViewBuilder $contentViewBuilder,
+        TemplateRenderer $templateRenderer
+    ) {
+        parent::__construct(self::$defaultName);
+        $this->contentViewBuilder = $contentViewBuilder;
+        $this->templateRenderer = $templateRenderer;
+    }
+
+    protected function configure(): void
+    {
+        $this->setDescription('Render the view of a content item')
+            ->addOption('content-id', 'c', InputOption::VALUE_OPTIONAL, 'Content ID')
+            ->addOption('location-id', 'l', InputOption::VALUE_OPTIONAL, 'Location ID')
+            ->addOption('view-type', 't', InputOption::VALUE_OPTIONAL, 'Location ID', 'line');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $contentId = $input->getOption('content-id');
+        $locationId = $input->getOption('location-id');
+        if (empty($contentId) && empty($locationId)) {
+            throw new \InvalidArgumentException('No Content ID nor Location ID given');
+        }
+
+        $viewParameters = [
+            'viewType' => $input->getOption('view-type'),
+            '_controller' => 'ibexa_content:viewAction',
+        ];
+
+        if (!empty($locationId)) {
+            $viewParameters['locationId'] = $locationId;
+        }
+        if (!empty($contentId)) {
+            $viewParameters['contentId'] = $contentId;
+        }
+
+        // build view
+        $contentView = $this->contentViewBuilder->buildView($viewParameters);
+
+        // generate the HTML using TemplateRenderer + ContentViewBuilder
+        $renderedView = $this->templateRenderer->render($contentView);
+
+        $output->writeln($renderedView);
+
+        return 0;
+    }
+}
---
+++ docs/templating/render_content/render_content_in_php.md
@@ -0,0 +1,46 @@
+---
+description: Render Content item from PHP and get the resulting HTML as a string.
+---
+
+# Render content in PHP
+
+While in PHP, you may need to render the view of a Content item (for example, for further treatment like PDF conversion, or because you're not in an HTML context).
+
+!!! caution
+
+    Avoid using PHP rendering in a controller as much as possible.
+    You can access a view directly through the route `/view/content/{contentId}/{viewType}[/{location}]`. For example, on a fresh installation, you can access `/view/content/52/line` which will return a small piece of HTML with a link to the content that could be used in Ajax.
+    If you need a controller to have additional information available in the template or to customize the `Response` object, define the controller in a [view configuration](template_configuration.md) as shown in [Controllers](controllers.md), enhance the View object and return it.
+
+The following example is a command outputting the render of a content for a view type in the terminal.
+It works only if the view doesn't refer to the HTTP request.
+It's compatible with the default installation views such as `line` or `embed`.
+To go further with this example, you could add some dedicated views not outputting HTML but, for example, plain text, [Symfony command styled text]([[= symfony_doc =]]/console/coloring.html) or Markdown.
+It doesn't work with a `full` view when the [Page layout](template_configuration.md#page-layout) uses `app.request.locale`, as is the case in the out-of-the-box configuration..
+
+Append to `config/services.yaml` the command as a service:
+
+```yaml
+services:
+    #…
+    App\Command\ViewCommand:
+        tags:
+            - { name: 'console.command', command: 'app:view' }
+```
+
+Create the command in `src/Command/ViewCommand.php`:
+
+```php hl_lines="59-63"
+[[= include_file('code_samples/front/render_content_in_php/src/Command/ViewCommand.php') =]]
+```
+
+!!! caution
+
+    As `Ibexa\Core\MVC\Symfony\View\Builder\ContentViewBuilder` and `Ibexa\Core\MVC\Symfony\View\Renderer\TemplateRenderer` are not part of the public API's `Ibexa\Contracts` namespace, they might change without notice.
+
+Use the command with some simple views:
+
+```bash
+php bin/console app:view --content-id=52
+php bin/console app:view --location-id=2 --view-type=embed
+```
