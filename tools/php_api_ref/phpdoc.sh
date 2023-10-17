a9f432a5d2c92642bc121c77deb6956e6a63fb9c
Adrien Dupuis <adrien.dupuis@ibexa.co>
2023-10-17 11:15:33 +0200

    phpdoc.sh: Document arguments and variables

--- tools/php_api_ref/phpdoc.sh
+++ tools/php_api_ref/phpdoc.sh
@@ -2,47 +2,45 @@
 
 set +x;
 
-AUTH_JSON=$1;
-OUTPUT_DIR=$2;
+AUTH_JSON=${1:-~/.composer/auth.json}; # Path to an auth.json file allowing to install the targeted edition and version
+OUTPUT_DIR=${2:-./docs/api/php_api/php_api_reference}; # Path to the directory where the built PHP API Reference is hosted
 
-FLAVOR='commerce';
-VERSION='4.6.*@dev';
-EDITIONS=(oss headless experience commerce);
-TMP_DXP_DIR=/tmp/ibexa-dxp-phpdoc;
-PHPDOC_CONF="$(pwd)/tools/php_api_ref/phpdoc.dist.xml";
-PHPDOC_DIR="$(pwd)/tools/php_api_ref/.phpdoc";
-if [ -z "$OUTPUT_DIR" ]; then
-  OUTPUT_DIR="$(pwd)/docs/api/php_api/php_api_reference";
-else
-  if [ ! -d $OUTPUT_DIR ]; then
-    echo -n "Creating ${OUTPUT_DIR}… ";
-    mkdir -p $OUTPUT_DIR;
-    if [ $? -eq 0 ]; then
-      echo 'OK';
-    else
-      exit 1;
-    fi;
+DXP_EDITION='commerce'; # Edition from and for which the Reference is built
+DXP_VERSION='4.6.*@dev'; # Version from and for which the Reference is built
+DXP_EDITIONS=(oss headless experience commerce); # Available editions ordered by ascending capabilities
+TMP_DXP_DIR=/tmp/ibexa-dxp-phpdoc; # Absolute path of the temporary directory in which Ibexa DXP will be installed and the PHP API Reference built
+PHPDOC_CONF="$(pwd)/tools/php_api_ref/phpdoc.dist.xml"; # Absolute path to phpDocumentor configuration file
+PHPDOC_DIR="$(pwd)/tools/php_api_ref/.phpdoc"; # Absolute path to phpDocumentor resource directory (containing the template set)
+PHPDOC_VERSION='3.3.1'; # Version of phpDocumentor used to build the Reference
+
+if [ ! -d $OUTPUT_DIR ]; then
+  echo -n "Creating ${OUTPUT_DIR}… ";
+  mkdir -p $OUTPUT_DIR;
+  if [ $? -eq 0 ]; then
+    echo 'OK';
+  else
+    exit 1;
   fi;
-  OUTPUT_DIR=$(readlink -f $OUTPUT_DIR);
 fi;
+OUTPUT_DIR=$(realpath $OUTPUT_DIR); # Transform to absolute path before changing the working directory
 
 rm -rf $TMP_DXP_DIR;
 mkdir -p $TMP_DXP_DIR;
 if [ $? -ne 0 ]; then
   exit 2;
 fi;
-cd $TMP_DXP_DIR;
+cd $TMP_DXP_DIR; # /!\ Change working directory (reason why all paths must be absolute)
 
-echo "Creating ibexa/$FLAVOR-skeleton:$VERSION project in ${TMP_DXP_DIR}…";
-composer create-project ibexa/$FLAVOR-skeleton:$VERSION . --no-interaction --no-install --ignore-platform-reqs --no-scripts;
+echo "Creating ibexa/$DXP_EDITION-skeleton:$DXP_VERSION project in ${TMP_DXP_DIR}…";
+composer create-project ibexa/$DXP_EDITION-skeleton:$DXP_VERSION . --no-interaction --no-install --ignore-platform-reqs --no-scripts;
 if [ -n "$AUTH_JSON" ]; then
   cp $AUTH_JSON ./;
 fi;
 composer install --no-interaction --ignore-platform-reqs --no-scripts;
 
-if [[ "$VERSION" == *".*"* ]]; then
-  VERSION=$(composer -n show ibexa/$FLAVOR | grep -E "^version" | cut -d 'v' -f 3);
-  echo "Obtained version: $VERSION";
+if [[ "$DXP_VERSION" == *".*"* ]]; then
+  DXP_VERSION=$(composer -n show ibexa/$DXP_EDITION | grep -E "^version" | cut -d 'v' -f 3);
+  echo "Obtained version: $DXP_VERSION";
 fi;
 
 echo -n 'Building package→edition map… ';
@@ -51,15 +49,15 @@ if [[ -f $map ]]; then
   rm $map;
 fi;
 echo "{% set package_edition_map = {" >> $map;
-for edition in ${EDITIONS[@]}; do
+for edition in ${DXP_EDITIONS[@]}; do
   echo -n "${edition}… ";
   while IFS= read -r line; do
     package=$(echo $line | cut -d '"' -f 2);
-    if [[ ! "${EDITIONS[*]}" =~ "${package/ibexa\//}" ]]; then
+    if [[ ! "${DXP_EDITIONS[*]}" =~ "${package/ibexa\//}" ]]; then
       echo "'$package': '$edition'," >> $map;
     fi;
-  done <<< "$(curl --no-progress-meter "https://raw.githubusercontent.com/ibexa/$edition/v$VERSION/composer.json" | jq .require | grep -E "(ibexa|ezsystems|silversolutions)")";
-  if [ "$edition" == "$FLAVOR" ]; then
+  done <<< "$(curl --no-progress-meter "https://raw.githubusercontent.com/ibexa/$edition/v$DXP_VERSION/composer.json" | jq .require | grep -E "(ibexa|ezsystems|silversolutions)")";
+  if [ "$edition" == "$DXP_EDITION" ]; then
     break;
   fi;
 done;
@@ -67,9 +65,9 @@ echo "} %}{% block content %}{% endblock %}" >> $map;
 echo 'OK';
 
 echo "Set and run phpDocumentor…";
-sed "s/version number=\".*\"/version number=\"$VERSION\"/" $PHPDOC_CONF > ./phpdoc.dist.xml;
+sed "s/version number=\".*\"/version number=\"$DXP_VERSION\"/" $PHPDOC_CONF > ./phpdoc.dist.xml;
 cp -R $PHPDOC_DIR ./;
-curl -LO "https://github.com/phpDocumentor/phpDocumentor/releases/download/v3.3.1/phpDocumentor.phar";
+curl -LO "https://github.com/phpDocumentor/phpDocumentor/releases/download/v$PHPDOC_VERSION/phpDocumentor.phar";
 php phpDocumentor.phar -t php_api_reference;
 if [ $? -eq 0 ]; then
   echo -n "Remove unneeded from phpDocumentor output… ";
