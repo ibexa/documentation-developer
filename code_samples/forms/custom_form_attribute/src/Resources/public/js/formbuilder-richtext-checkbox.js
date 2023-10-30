197cae7b051547eda08465342ca2be84f92763f0
Tomasz Dąbrowski <64841871+dabrt@users.noreply.github.com>
2023-10-30 11:26:57 +0100

    IBX-6654 - Fix code in create form attrib tutorial

--- code_samples/forms/custom_form_attribute/src/Resources/public/js/formbuilder-richtext-checkbox.js
+++ code_samples/forms/custom_form_attribute/src/Resources/public/js/formbuilder-richtext-checkbox.js
@@ -1,9 +1,20 @@
-(function(global, doc, eZ, $) {
-    const richtext       = new global.eZ.BaseRichText();
+(function (global, doc, eZ) {
+    global.addEventListener('load', (event) => {
+        const richtext = new eZ.BaseRichText();
 
-    // Enable editor in all ez-data-source divs
-    doc.querySelectorAll('.ez-data-source').forEach((ezDataSource) => {
-        const richtextContainer = ezDataSource.querySelector(".ez-data-source__richtext");
-        const alloyEditor    = richtext.init(richtextContainer);
+        // Enable editor in all ez-data-source divs
+        doc.querySelectorAll('.ez-data-source').forEach(
+            (ezDataSource) => {
+                const richtextContainer = ezDataSource.querySelector(
+                    '.ez-data-source__richtext'
+                );
+
+                if (!richtextContainer || richtextContainer.hasAttribute('contenteditable')) {
+                    return;
+                }
+
+                const CKEditor = richtext.init(richtextContainer);
+            }
+        );
     });
-})(window, window.document, window.eZ, window.jQuery);
+})(window, window.document, window.eZ);
