a20c9e2ad8d5d5421582661775a1e389be67467d
adriendupuis <adriendupuis@users.noreply.github.com>
2024-02-13 16:48:21 +0000

    PHP CS Fixes

--- code_samples/recent_activity/src/Command/MonitorRecentContentCreationCommand.php
+++ code_samples/recent_activity/src/Command/MonitorRecentContentCreationCommand.php
@@ -11,7 +11,6 @@ use Ibexa\Contracts\Core\Repository\UserService;
 use Ibexa\Contracts\Core\Repository\Values\Content\Content;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
