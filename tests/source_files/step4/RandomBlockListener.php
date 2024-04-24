f3c9f7fdaddd62dd18dc784113859bb47950dd10
mnocon <mnocon@users.noreply.github.com>
2024-04-24 19:57:15 +0000

    PHP CS Fixes

--- tests/source_files/step4/RandomBlockListener.php
+++ tests/source_files/step4/RandomBlockListener.php
@@ -2,6 +2,7 @@
 
 /**
  * @copyright Copyright (C) Ibexa. All rights reserved.
+ *
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
 declare(strict_types=1);
