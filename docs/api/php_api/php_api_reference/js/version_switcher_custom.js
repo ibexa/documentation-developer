e5458f0a6400c9259a6a7443ba19c54b2f36fec5
Michał Grabowski <michal.grabowski@ibexa.co>
2024-04-25 12:15:00 +0200

    dev 7

--- docs/api/php_api/php_api_reference/js/version_switcher_custom.js
+++ docs/api/php_api/php_api_reference/js/version_switcher_custom.js
@@ -1,38 +1,24 @@
 (function(global, doc) {
-    const initializeSwitcher = (addedNode) => {
-        setTimeout(() => {
-            console.log(addedNode);
-            if (!addedNode || addedNode.tagName !== 'READTHEDOCS-FLYOUT' || !addedNode.shadowRoot) {
-                return;
-            }
-            console.log(addedNode.shadowRoot);
-            debugger;
-            console.log(addedNode.shadowRoot.querySelector('dl.versions'));
-            debugger;
+    let interval = null;
+    const initializeSwitcher = () => {
+        const injectedNode = doc.getElementsByTagName('readthedocs-flyout')[0];
 
-            const switcherWrapper = document.querySelector('.md-header__switcher .version-switcher');
-            const switcherList = switcherWrapper.querySelector('.switcher__list');
-            const currentVersionNode = switcherWrapper.querySelector('.rst-current-version');
-            const versionsList = addedNode.shadowRoot.querySelector('dl.versions');
-            const version = addedNode.querySelector('.switcher__list dl.versions dd strong a')?.innerText;
+        if (!injectedNode || !injectedNode.shadowRoot) {
+            return;
+        }
 
-            versionsList.append(...Array.from(versionsList.childNodes).reverse());
-            switcherList.appendChild(versionsList);
-            currentVersionNode.innerText = version ?? 'Change version';
-        }, 100);
+        const switcherWrapper = document.querySelector('.md-header__switcher .version-switcher');
+        const switcherList = switcherWrapper.querySelector('.switcher__list');
+        const currentVersionNode = switcherWrapper.querySelector('.rst-current-version');
+        const versionsList = injectedNode.shadowRoot.querySelector('dl.versions');
+        const version = injectedNode.querySelector('.switcher__list dl.versions dd strong a')?.innerText;
+
+        versionsList.append(...Array.from(versionsList.childNodes).reverse());
+        switcherList.appendChild(versionsList);
+        currentVersionNode.innerText = version ?? 'Change version';
+
+        clearInterval(interval);
     }
-    const observer = new MutationObserver((mutationList) => {
-        mutationList.forEach((mutation) => {
-            mutation.addedNodes.forEach((addedNode) => initializeSwitcher(addedNode));
-        });
-    });
-    const injectedNode = doc.getElementsByTagName('readthedocs-flyout');
-
-    observer.observe(doc.body, {
-        childList: true,
-    });
-
-    setTimeout(() => {
-        initializeSwitcher(injectedNode[0]);
-    }, 100);
+    
+    interval = setInterval(initializeSwitcher, 100);
 })(window, window.document);
