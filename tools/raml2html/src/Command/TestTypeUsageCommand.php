cdb1f730e2eeaec15b156dbc85e598a6f43eaba3
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2024-04-29 16:24:01 +0200

    TestTypeUsageCommand.php: Fix recursion

--- tools/raml2html/src/Command/TestTypeUsageCommand.php
+++ tools/raml2html/src/Command/TestTypeUsageCommand.php
@@ -10,6 +10,10 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 final class TestTypeUsageCommand extends Command
 {
+    private array $definedTypes;
+
+    private array $usedByOtherTypeTypeList;
+
     /**
      * {@inheritdoc}
      */
@@ -25,11 +29,11 @@ final class TestTypeUsageCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $definedTypes = yaml_parse_file('docs/api//rest_api/rest_api_reference/input/ibexa-types.raml');
+        $this->definedTypes = yaml_parse_file('docs/api//rest_api/rest_api_reference/input/ibexa-types.raml');
         $usedByRouteTypeList = [];
-        $usedByOtherTypeTypeList = [];
+        $this->usedByOtherTypeTypeList = [];
 
-        foreach ($definedTypes as $type => $typeDefinition) {
+        foreach ($this->definedTypes as $type => $typeDefinition) {
             $usageFileList = shell_exec("grep 'type: $type' -R docs/api/rest_api/rest_api_reference/input | grep -v examples | grep -v ibexa-types.raml");
             if (!empty($usageFileList)) {
                 $usedByRouteTypeList[] = $type;
@@ -37,42 +41,53 @@ final class TestTypeUsageCommand extends Command
         }
 
         foreach ($usedByRouteTypeList as $usedByRouteType) {
-            $definedType = $definedTypes[$usedByRouteType];
+            $definedType = $this->definedTypes[$usedByRouteType];
             $usedType = str_ends_with($definedType['type'], '[]') ? substr($definedType['type'], 0, -2) : $definedType['type'];
-            if (!in_array($usedType, $usedByOtherTypeTypeList)) {
-                $usedByOtherTypeTypeList[] = $usedType;
+            if (!in_array($usedType, $this->usedByOtherTypeTypeList)) {
+                $this->usedByOtherTypeTypeList[] = $usedType;
             }
-            if (array_key_exists('properties', $definedType)) {
-                foreach ($definedType['properties'] as $property => $propertyDefinition) {
-                    if (is_array($propertyDefinition)) {
-                        if (array_key_exists('type', $propertyDefinition)) {
-                            if ('array' === $propertyDefinition['type']) {
-                                $usedType = $propertyDefinition['items'];
-                            } elseif (str_ends_with($propertyDefinition['type'], '[]')) {
-                                $usedType = substr($propertyDefinition['type'], 0, -2);
-                            } else {
-                                $usedType = $propertyDefinition['type'];
-                            }
-                        }
-                    } else {
-                        if (str_ends_with($propertyDefinition, '[]')) {
-                            $usedType = substr($propertyDefinition, 0, -2);
+            $this->exploreProperties($definedType);
+        }
+
+        $usedTypeList = array_merge($usedByRouteTypeList, $this->usedByOtherTypeTypeList);
+        $unusedTypeList = array_values(array_diff(array_keys($this->definedTypes), $usedTypeList));
+        sort($unusedTypeList);
+        dump($unusedTypeList);
+
+        return Command::SUCCESS;
+    }
+
+    private function exploreProperties(array $definedType)
+    {
+        if (array_key_exists('properties', $definedType)) {
+            foreach ($definedType['properties'] as $property => $propertyDefinition) {
+                $usedType = null;
+                if (is_array($propertyDefinition)) {
+                    if (array_key_exists('type', $propertyDefinition)) {
+                        if ('array' === $propertyDefinition['type']) {
+                            $usedType = $propertyDefinition['items'];
+                        } elseif (array_key_exists('properties', $propertyDefinition)) {
+                            $this->exploreProperties($propertyDefinition, $this->usedByOtherTypeTypeList);
+                        } elseif (str_ends_with($propertyDefinition['type'], '[]')) {
+                            $usedType = substr($propertyDefinition['type'], 0, -2);
                         } else {
-                            $usedType = $propertyDefinition;
+                            $usedType = $propertyDefinition['type'];
                         }
                     }
-                    if (!in_array($usedType, $usedByOtherTypeTypeList)) {
-                        $usedByOtherTypeTypeList[] = $usedType;
+                } else {
+                    if (str_ends_with($propertyDefinition, '[]')) {
+                        $usedType = substr($propertyDefinition, 0, -2);
+                    } else {
+                        $usedType = $propertyDefinition;
+                    }
+                }
+                if (null !== $usedType && !in_array($usedType, $this->usedByOtherTypeTypeList)) {
+                    $this->usedByOtherTypeTypeList[] = $usedType;
+                    if (array_key_exists($usedType, $this->definedTypes)) {
+                        $this->exploreProperties($this->definedTypes[$usedType]);
                     }
                 }
             }
         }
-
-        $usedTypeList = array_merge($usedByRouteTypeList, $usedByOtherTypeTypeList);
-        $unusedTypeList = array_diff(array_keys($definedTypes), $usedTypeList);
-
-        dump($unusedTypeList);
-
-        return 0;
     }
 }
