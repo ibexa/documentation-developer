a436017eb30ab4dbfa0eea6e2a770fced95b6634
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2024-05-02 18:43:42 +0200

    Draft a training template

--- tools/trainings.downloads.sh
+++ tools/trainings.downloads.sh
@@ -1,3 +1,9 @@
-cd docs/trainings/a_training_topic/a_late_training/download/a_late_training.start/
+cd docs/trainings/a_training_topic/a_late_training/download/start/;
+rm -f ../a_late_training.start.zip;
 zip -r ../a_late_training.start.zip ./* ;
+cd -;
+
+cd docs/trainings/a_training_topic/a_late_training/download/step_01/;
+rm -f ../a_late_training.start.zip;
+zip -r ../a_late_training.step_01.zip ./* ;
 cd -;
