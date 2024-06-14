8053f7f3122a8321e4ea821423d599912d440e6c
Michał Grabowski <michal.grabowski@ibexa.co>
2024-06-14 13:08:01 +0200

    built

--- tools/php_api_ref/.phpdoc/template/js/copy_to_clipboard.js
+++ tools/php_api_ref/.phpdoc/template/js/copy_to_clipboard.js
@@ -1,9 +1,38 @@
 (function(global, doc, navigator) {
     const copyBtns = doc.querySelectorAll('.copy-to-clipboard');
+    const copiedPopover = doc.querySelector('.popover--copied');
+    const copiedPopoverOffset = copiedPopover.offsetWidth / 2;
+    const POPOVER_HIDE_TIMEOUT = 2000;
+    let popoverTimeout = null;
 
     copyBtns.forEach((copyBtn) => {
         copyBtn.addEventListener('click', (event) => {
-            navigator.clipboard.writeText(event.target.dataset.copyValue);
+            const { offsetLeft, offsetTop, offsetWidth, offsetHeight, dataset } = event.target;
+            const popoverX = offsetLeft + offsetWidth / 2 - copiedPopoverOffset;
+            const popoverY = offsetTop + offsetHeight + 8;
+            const hidePopover = () => {
+                copiedPopover.style.opacity = 0;
+
+                clearTimeout(popoverTimeout);
+                copiedPopover.classList.remove('popover--visible');
+                copiedPopover.removeEventListener('transitionend', hidePopover, false);
+            }
+
+            hidePopover();
+
+            copiedPopover.style.left = `${popoverX}px`;
+            copiedPopover.style.top = `${popoverY}px`;
+            copiedPopover.style.opacity = 1;
+
+            copiedPopover.classList.add('popover--visible');
+
+            navigator.clipboard.writeText(dataset.copyValue);
+
+            popoverTimeout = setTimeout(() => {
+                copiedPopover.style.opacity = 0;
+
+                copiedPopover.addEventListener('transitionend', hidePopover, false);
+            }, POPOVER_HIDE_TIMEOUT);
         });
     });
 })(window, window.document, window.navigator);
