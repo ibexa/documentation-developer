585922be9dfe5b3b4c5618cec010ecdeefef6482
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2024-08-26 11:52:47 +0200

    AttributeCommand.php → ProductAttributeCommand.php

--- code_samples/api/product_catalog/src/Command/ProductAttributeCommand.php
+++ code_samples/api/product_catalog/src/Command/ProductAttributeCommand.php
@@ -13,7 +13,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-final class AttributeCommand extends Command
+final class ProductAttributeCommand extends Command
 {
     private AttributeGroupServiceInterface $attributeGroupService;
 
