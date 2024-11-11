8565a064469786d845c5e656a6ed2eeb105fc032
Marek Nocoń <mnocon@users.noreply.github.com>
2024-11-11 19:02:48 +0100

    Removed obsolete files

--- code_samples/ai_actions/src/REST/Value/AudioText.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace App\AI\REST\Value;
-
-use Ibexa\ConnectorAi\REST\Value\RestActionResponse;
-
-final class AudioText extends RestActionResponse
-{
-}
--- code_samples/ai_actions/src/REST/Value/TranscribeAudioAction.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace App\AI\REST\Value;
-
-use Ibexa\ConnectorAi\REST\Value\RestAction;
-
-final class TranscribeAudioAction extends RestAction
-{
-}
