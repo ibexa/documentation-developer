9ec43a8794c631f0cd260345f315a9076ef8d9eb
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2024-12-04 17:14:34 +0100

    custom.js: No search result highlight on keyboard nav

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -144,6 +144,7 @@ $(document).ready(function() {
         debug: false,
     });
     search.autocomplete.on('autocomplete:updated', event => {
+        $('.ds-cursor').removeClass('ds-cursor');
         const searchedText = $('#search_input')[0].value.trim();
         const separatorText = '›';
         const separatorClass = 'aa-suggestion-title-separator';
@@ -170,8 +171,9 @@ $(document).ready(function() {
     });
 
     $(document).on('keydown keypress', 'form.md-search__form', function(event) {
-        if (event.keyCode == 13) {
+        if (-1 != $.inArray(event.key, ['Enter', 'ArrowDown', 'ArrowUp'])) {
             event.preventDefault();
+            $('.ds-cursor').removeClass('ds-cursor');
 
             return false;
         }
