07a43920134bfb12a49df9ab034015fce4491fa5
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2024-12-10 20:50:11 +0100

    Update CameraManufacturerVisitor signatures

--- code_samples/search/elasticsearch/src/Query/Criterion/Elasticsearch/CameraManufacturerVisitor.php
+++ code_samples/search/elasticsearch/src/Query/Criterion/Elasticsearch/CameraManufacturerVisitor.php
@@ -4,22 +4,23 @@ declare(strict_types=1);
 
 namespace App\Query\Criterion\Elasticsearch;
 
-use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion;
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\CriterionInterface;
 use Ibexa\Contracts\Elasticsearch\Query\CriterionVisitor;
 use Ibexa\Contracts\Elasticsearch\Query\LanguageFilter;
 
 final class CameraManufacturerVisitor implements CriterionVisitor
 {
-    public function supports(Criterion $criterion, LanguageFilter $languageFilter): bool
+    public function supports(CriterionInterface $criterion, LanguageFilter $languageFilter): bool
     {
         return $criterion instanceof CameraManufacturerCriterion;
     }
 
-    public function visit(CriterionVisitor $dispatcher, Criterion $criterion, LanguageFilter $languageFilter): array
+    public function visit(CriterionVisitor $dispatcher, CriterionInterface $criterion, LanguageFilter $languageFilter): array
     {
+        /** @var \Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion $criterion */
         return [
             'terms' => [
-                'exif_camera_manufacturer_id' => (array)$criterion->value,
+                'exif_camera_manufacturer_id' => property_exists($criterion, 'value')?(array)$criterion->value:[],
             ],
         ];
     }
--- code_samples/search/solr/src/Query/Criterion/Solr/CameraManufacturerVisitor.php
+++ code_samples/search/solr/src/Query/Criterion/Solr/CameraManufacturerVisitor.php
@@ -4,23 +4,24 @@ declare(strict_types=1);
 
 namespace App\Query\Criterion\Solr;
 
-use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion;
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\CriterionInterface;
 use Ibexa\Contracts\Solr\Query\CriterionVisitor;
 
 final class CameraManufacturerVisitor extends CriterionVisitor
 {
-    public function canVisit(Criterion $criterion)
+    public function canVisit(CriterionInterface $criterion)
     {
         return $criterion instanceof CameraManufacturerCriterion;
     }
 
-    public function visit(Criterion $criterion, CriterionVisitor $subVisitor = null)
+    public function visit(CriterionInterface $criterion, CriterionVisitor $subVisitor = null)
     {
+        /** @var \Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion $criterion */
         $expressions = array_map(
             function ($value): string {
                 return 'exif_camera_manufacturer_id:"' . $this->escapeQuote((string) $value) . '"';
             },
-            $criterion->value
+            property_exists($criterion, 'value')?(array)$criterion->value:[]
         );
 
         return '(' . implode(' OR ', $expressions) . ')';
