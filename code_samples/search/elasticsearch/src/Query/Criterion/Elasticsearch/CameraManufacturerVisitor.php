7e59b8fbd2d559e23b294f312db5f53c4892f657
Marek Nocoń <mnocon@users.noreply.github.com>
2024-12-10 18:46:17 +0100

    [v5] Moved from Criterion to CriterionInterface

--- code_samples/search/elasticsearch/src/Query/Criterion/Elasticsearch/CameraManufacturerVisitor.php
+++ code_samples/search/elasticsearch/src/Query/Criterion/Elasticsearch/CameraManufacturerVisitor.php
@@ -4,18 +4,18 @@ declare(strict_types=1);
 
 namespace App\Query\Criterion\Elasticsearch;
 
-use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion;
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\CriterionInterface;
 use Ibexa\Contracts\Elasticsearch\Query\CriterionVisitor;
 use Ibexa\Contracts\Elasticsearch\Query\LanguageFilter;
 
 final class CameraManufacturerVisitor implements CriterionVisitor
 {
-    public function supports(Criterion $criterion, LanguageFilter $languageFilter): bool
+    public function supports(CriterionInterface $criterion, LanguageFilter $languageFilter): bool
     {
         return $criterion instanceof CameraManufacturerCriterion;
     }
 
-    public function visit(CriterionVisitor $dispatcher, Criterion $criterion, LanguageFilter $languageFilter): array
+    public function visit(CriterionVisitor $dispatcher, CriterionInterface $criterion, LanguageFilter $languageFilter): array
     {
         return [
             'terms' => [
--- code_samples/search/solr/src/Query/Criterion/Solr/CameraManufacturerVisitor.php
+++ code_samples/search/solr/src/Query/Criterion/Solr/CameraManufacturerVisitor.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace App\Query\Criterion\Solr;
 
-use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion;
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\CriterionInterface;
 use Ibexa\Contracts\Solr\Query\CriterionVisitor;
 
 final class CameraManufacturerVisitor extends CriterionVisitor
 {
-    public function canVisit(Criterion $criterion)
+    public function canVisit(CriterionInterface $criterion)
     {
         return $criterion instanceof CameraManufacturerCriterion;
     }
 
-    public function visit(Criterion $criterion, CriterionVisitor $subVisitor = null)
+    public function visit(CriterionInterface $criterion, CriterionVisitor $subVisitor = null)
     {
         $expressions = array_map(
             function ($value): string {
