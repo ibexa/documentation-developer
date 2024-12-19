177c2d898463983390976a92a8f19a5571d9031d
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2024-12-19 09:24:10 +0100

    Generate PHP API Ref HTML

---
+++ docs/api/php_api/php_api_reference/js/external-links.js
@@ -0,0 +1,12 @@
+//TODO:         window.symfonyVersion = '{{ symfony_version }}';
+//TODO: <script src="js/external-links.js"></script>
+$(() => {
+    $('abbr').each((index, element) => {
+        let $this = $(element);
+        let fqcn = $this.attr('title');
+        if (fqcn.startsWith('\\Symfony\\') && 'a' != $this.parent().prop('tagName')) {
+            let href = 'https://github.com/symfony/symfony/blob/' + symfonyVersion + '/src' + fqcn.replaceAll('\\', '/') + '.php';
+            $this.wrap('<a href="' + href + '" class="external">');
+        }
+    });
+})
