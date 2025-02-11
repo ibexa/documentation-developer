70233c515dbdb87257f58fb0077fedf5f2d82d82
mnocon <mnocon@users.noreply.github.com>
2025-02-11 18:56:25 +0000

    PHP CS Fixes

--- code_samples/back_office/search/src/Query/DateTimeAttributeRangeQuery.php
+++ code_samples/back_office/search/src/Query/DateTimeAttributeRangeQuery.php
@@ -1,11 +1,10 @@
-<?php
+<?php declare(strict_types=1);
 
 use DateTimeImmutable;
-use Ibexa\Contracts\CoreSearch\Values\Query\Criterion\FieldValueCriterion;
 use Ibexa\Contracts\ProductCatalog\Values\Product\ProductQuery;
 use Ibexa\Contracts\ProductCatalogDateTimeAttribute\Search\Criterion\DateTimeAttributeRange;
 
 $query = new ProductQuery();
 $query->setFilter(new DateTimeAttributeRange('event_date', new DateTimeImmutable('2025-01-01')));
-/** @var \Ibexa\Contracts\ProductCatalog\ProductServiceInterface $productService*/
+/** @var \Ibexa\Contracts\ProductCatalog\ProductServiceInterface $productService */
 $results = $productService->findProducts($query);
