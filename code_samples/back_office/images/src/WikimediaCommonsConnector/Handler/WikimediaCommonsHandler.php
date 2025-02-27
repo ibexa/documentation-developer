6e0c24538937ccd3ce6616f753f3b384f7b38a87
dabrt <dabrt@users.noreply.github.com>
2025-02-27 19:39:38 +0000

    PHP & JS CS Fixes

--- code_samples/back_office/images/src/WikimediaCommonsConnector/Handler/WikimediaCommonsHandler.php
+++ code_samples/back_office/images/src/WikimediaCommonsConnector/Handler/WikimediaCommonsHandler.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace WikimediaCommonsConnector\Handler;
 
@@ -17,9 +17,9 @@ class WikimediaCommonsHandler implements HandlerInterface
     public function search(Query $query, int $offset = 0, int $limit = 20): AssetSearchResult
     {
         $searchUrl = 'https://commons.wikimedia.org/w/api.php?action=query&list=search&format=json&srnamespace=6'
-            .'&srsearch='.urlencode($query->getPhrase())
-            .'&sroffset='.$offset
-            .'&srlimit='.$limit
+            . '&srsearch=' . urlencode($query->getPhrase())
+            . '&sroffset=' . $offset
+            . '&srlimit=' . $limit
         ;
         $response = json_decode(file_get_contents($searchUrl), true);
 
@@ -35,7 +35,7 @@ class WikimediaCommonsHandler implements HandlerInterface
     public function fetchAsset(string $id): Asset
     {
         $metadataUrl = 'https://commons.wikipedia.org/w/api.php?action=query&prop=imageinfo&iiprop=extmetadata&format=json'
-            .'&titles=File%3a'.urlencode($id)
+            . '&titles=File%3a' . urlencode($id)
         ;
         $response = json_decode(file_get_contents($metadataUrl), true);
         $imageInfo = array_values($response['query']['pages'])[0]['imageinfo'][0]['extmetadata'];
@@ -43,7 +43,7 @@ class WikimediaCommonsHandler implements HandlerInterface
         return new Asset(
             new AssetIdentifier($id),
             new AssetSource('commons'),
-            new AssetUri('https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/'.urlencode($id)),
+            new AssetUri('https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/' . urlencode($id)),
             new AssetMetadata([
                 'page_url' => "https://commons.wikimedia.org/wiki/File:$id",
                 'author' => array_key_exists('Artist', $imageInfo) ? $imageInfo['Artist']['value'] : null,
