1e76897a185af5acb41999b8053168c64e64827c
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2025-03-31 12:49:39 +0200

    Add REST controller to doc

--- code_samples/api/rest_api/src/Rest/Controller/DefaultController.php
+++ code_samples/api/rest_api/src/Rest/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Rest\Controller;
 
 use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
 use ApiPlatform\OpenApi\Factory\OpenApiFactory;
 use ApiPlatform\OpenApi\Model;
 use App\Rest\Values\Greeting;
@@ -12,18 +13,146 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\SerializerInterface;
 
-#[Get(
+#[Post(
     uriTemplate: '/greet',
     extraProperties: [OpenApiFactory::OVERRIDE_OPENAPI_RESPONSES => false],
     openapi: new Model\Operation(
-        summary: 'TODO: Greet',
-        description: 'TODO',
+        summary: 'Greet',
+        description: 'Greets a recipient with a salutation',
         tags: [
-            'User',
+            'App',
         ],
+        parameters: [
+            new Model\Parameter(
+                name: 'Content-Type',
+                in: 'header',
+                required: false,
+                description: 'The greeting input schema encoded in XML or JSON.',
+                schema: [
+                    'type' => 'string',
+                ],
+            ),
+            new Model\Parameter(
+                name: 'Accept',
+                in: 'header',
+                required: true,
+                description: 'If set, the greeting is returned in XML or JSON format.',
+                schema: [
+                    'type' => 'string',
+                ],
+            ),
+        ],
+        requestBody: new Model\RequestBody(
+            required: false,
+            content: new \ArrayObject([
+                'application/vnd.ibexa.api.GreetingInput+xml' => [
+                    'schema' => [
+                        'type' => 'object',
+                        'xml' => [
+                            'name' => 'GreetingInput',
+                            'wrapped' => false,
+                        ],
+                        'properties' => [
+                            'salutation' => [
+                                'type' => 'string',
+                                'required' => false,
+                            ],
+                            'recipient' => [
+                                'type' => 'string',
+                                'required' => false,
+                            ],
+                        ],
+                    ],
+                    'example' => [
+                        'salutation' => 'Good morning',
+                    ],
+                ],
+                'application/vnd.ibexa.api.GreetingInput+json' => [
+                    'schema' => [
+                        'type' => 'object',
+                        'properties' => [
+                            'GreetingInput' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'salutation' => [
+                                        'type' => 'string',
+                                        'required' => false,
+                                    ],
+                                    'recipient' => [
+                                        'type' => 'string',
+                                        'required' => false,
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                    'example' => [
+                        'GreetingInput' => [
+                            'salutation' => 'Good day',
+                            'recipient' => 'Earth',
+                        ],
+                    ],
+                ],
+            ]),
+        ),
         responses: [
             Response::HTTP_OK => [
-                'description' => 'TODO',
+                'description' => 'OK - Return a greeting',
+                'content' => [
+                    'application/vnd.ibexa.api.Greeting+xml' => [
+                        'schema' => [
+                            'xml' => [
+                                'name' => 'Greeting',
+                                'wrapped' => false,
+                            ],
+                            'properties' => [
+                                'salutation' => [
+                                    'type' => 'string',
+                                ],
+                                'recipient' => [
+                                    'type' => 'string',
+                                ],
+                                'sentence' => [
+                                    'type' => 'string',
+                                    'description' => 'Composed sentence using salutation and recipient.'
+                                ]
+                            ],
+                        ],
+                        'example' => [
+                            'salutation' => 'Good morning',
+                            'recipient' => 'World',
+                            'sentence' => 'Good Morning World',
+                        ],
+                    ],
+                    'application/vnd.ibexa.api.Greeting+json' => [
+                        'schema' => [
+                            'type' => 'object',
+                            'properties' => [
+                                'Greeting' => [
+                                    'type' => 'object',
+                                    'properties' => [
+                                        'salutation' => [
+                                            'type' => 'string',
+                                        ],
+                                        'recipient' => [
+                                            'type' => 'string',
+                                        ],
+                                        'sentence' => [
+                                            'type' => 'string',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                        'example' => [
+                            'Greeting' => [
+                                'salutation' => 'Good day',
+                                'recipient' => 'Earth',
+                                'sentence' => 'Good day Earth',
+                            ],
+                        ],
+                    ],
+                ],
             ],
         ],
     ),
