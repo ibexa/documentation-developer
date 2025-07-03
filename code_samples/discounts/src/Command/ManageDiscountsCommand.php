4aaaca25c00d878c463734eda3a0d766e0dee5a5
Marek Nocoń <mnocon@users.noreply.github.com>
2025-07-03 09:25:52 +0200

    Update code_samples/discounts/src/Command/ManageDiscountsCommand.php
    Co-authored-by: Konrad Oboza <konrad.oboza@ibexa.co>

--- code_samples/discounts/src/Command/ManageDiscountsCommand.php
+++ code_samples/discounts/src/Command/ManageDiscountsCommand.php
@@ -79,7 +79,11 @@ final class ManageDiscountsCommand extends Command
                 new IsInRegions(['germany', 'france']),
                 new IsProductInArray(['product-1', 'product-2']),
                 new IsInCurrency('EUR'),
-                new IsValidDiscountCode($discountCode->getCode(), $discountCode->getGlobalLimit(), $discountCode->getUsedLimit()),
+                new IsValidDiscountCode(
+                    $discountCode->getCode(),
+                    $discountCode->getGlobalLimit(),
+                    $discountCode->getUsedLimit()
+                ),
             ])
             ->setTranslations([
                 new DiscountTranslationStruct('eng-GB', 'Discount name', 'This is a discount description', 'Promotion Label', 'Promotion Description'),
