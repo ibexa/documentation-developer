c83c9a2231bf364b43039ae8b631e255f29d419c
julitafalcondusza <julitafalcondusza@users.noreply.github.com>
2025-07-14 10:22:50 +0000

    PHP & JS CS Fixes

--- code_samples/back_office/search/src/Query/SymbolAttributeTypeQuery.php
+++ code_samples/back_office/search/src/Query/SymbolAttributeTypeQuery.php
@@ -1,9 +1,9 @@
-<?php
+<?php declare(strict_types=1);
 
 use Ibexa\Contracts\ProductCatalog\Values\Product\ProductQuery;
 use Ibexa\Contracts\ProductCatalogSymbolAttribute\Search\Criterion\SymbolAttribute;
 
 $query = new ProductQuery();
 $query->setFilter(new SymbolAttribute('ean', ['5023920187205']));
-/** @var \Ibexa\Contracts\ProductCatalog\ProductServiceInterface $productService*/
+/** @var \Ibexa\Contracts\ProductCatalog\ProductServiceInterface $productService */
 $results = $productService->findProducts($query);
