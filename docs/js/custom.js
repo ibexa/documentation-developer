8483b3950f167f0f69feebbdb14bafb463c17a2b
Marek Nocoń <mnocon@users.noreply.github.com>
2025-07-30 10:09:45 +0200

    Combine latest and 5.0 entries in the switcher

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -76,6 +76,14 @@ $(document).ready(function() {
             const allVersions = [...document.querySelectorAll('.switcher__list .versions dd')];
             const olderVersions = document.querySelector('#older-versions');
 
+            // Merge "5.0" and "latest" entries into "5.0 (latest)"
+            const latestVersion = allVersions.find(v => v.textContent.trim() === 'latest');
+            const version50 = allVersions.find(v => v.textContent.trim() === '5.0');
+            
+            const version50Link = version50.querySelector('a');
+            version50Link.textContent = '5.0 (latest)';
+            latestVersion.hidden = true;
+
             if (eolVersions.length > 0) {
                 olderVersions.hidden = false;
             }
