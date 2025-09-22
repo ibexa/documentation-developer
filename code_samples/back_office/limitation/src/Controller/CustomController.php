c2ac2fa7f36d9e1a70a574bdadba76f0e650a470
Marek Nocoń <mnocon@users.noreply.github.com>
2025-09-22 18:53:37 +0200

    Run Rector on CustomController

--- code_samples/back_office/limitation/src/Controller/CustomController.php
+++ code_samples/back_office/limitation/src/Controller/CustomController.php
@@ -6,27 +6,18 @@ use App\Security\Limitation\CustomLimitationValue;
 use Ibexa\Contracts\AdminUi\Controller\Controller;
 use Ibexa\Contracts\AdminUi\Permission\PermissionCheckerInterface;
 use Ibexa\Contracts\Core\Repository\PermissionResolver;
+use Ibexa\Contracts\User\Controller\RestrictedControllerInterface;
 use Ibexa\Core\MVC\Symfony\Security\Authorization\Attribute;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class CustomController extends Controller
+class CustomController extends Controller implements RestrictedControllerInterface
 {
-    // ...
-    /** @var \Ibexa\Contracts\Core\Repository\PermissionResolver */
-    private $permissionResolver;
-
-    /** @var \Ibexa\Contracts\AdminUi\Permission\PermissionCheckerInterface */
-    private $permissionChecker;
-
     public function __construct(
         // ...,
-        PermissionResolver $permissionResolver,
-        PermissionCheckerInterface $permissionChecker
+        private readonly PermissionResolver $permissionResolver,
+        private readonly PermissionCheckerInterface $permissionChecker
     ) {
-        // ...
-        $this->permissionResolver = $permissionResolver;
-        $this->permissionChecker = $permissionChecker;
     }
 
     // Controller actions...
@@ -49,7 +40,6 @@ class CustomController extends Controller
 
     public function performAccessCheck(): void
     {
-        parent::performAccessCheck();
         $this->denyAccessUnlessGranted(new Attribute('custom_module', 'custom_function_2'));
     }
 }
