b11790dafda2532b8bcfdb9647358975278de40b
mnocon <mnocon@users.noreply.github.com>
2025-09-22 16:47:07 +0000

    PHP & JS CS Fixes

--- code_samples/back_office/notifications/src/Notification/MyRenderer.php
+++ code_samples/back_office/notifications/src/Notification/MyRenderer.php
@@ -34,7 +34,7 @@ class MyRenderer implements NotificationRenderer, TypedNotificationRendererInter
 
         return null;
     }
-    
+
     public function getTypeLabel(): string
     {
         return /** @Desc("Workflow stage changed") */
