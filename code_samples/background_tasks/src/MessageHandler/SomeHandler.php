cd0a7741c1f5288d7be72d045aa0da788289f236
Tomasz Dąbrowski <64841871+dabrt@users.noreply.github.com>
2025-10-03 18:20:38 +0200

    Modify the handler

--- code_samples/background_tasks/src/MessageHandler/SomeHandler.php
+++ code_samples/background_tasks/src/MessageHandler/SomeHandler.php
@@ -3,8 +3,9 @@
 namespace App\MessageHandler;
 
 use App\Message\SomeMessage;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
-final class SomeHandler
+final class SomeHandler implements MessageHandlerInterface
 {
     public function __invoke(SomeMessage $message): void
     {
