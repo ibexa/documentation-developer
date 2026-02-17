442db3afad5f7c4d6bf14c4d8325bf34ccf9d782
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2026-02-17 15:24:13 +0100

    Revert "add-to-shopping-list.ts: Test global.ibexaShoppingList"
    This reverts commit d6cc03a42e642c971be98321311cb6d58a936c54.

--- code_samples/shopping_list/add_to_shopping_list/assets/js/add-to-shopping-list.ts
+++ code_samples/shopping_list/add_to_shopping_list/assets/js/add-to-shopping-list.ts
@@ -6,8 +6,8 @@ import { AddToShoppingList } from '@ibexa-shopping-list/src/bundle/Resources/pub
 import { ShoppingListsList } from '@ibexa-shopping-list/src/bundle/Resources/public/js/component/shopping.lists.list';
 
 (function (global: Window, doc: Document) {
-    const shoppingList = global.ibexaShoppingList ?? new ShoppingList();
-    if (!global.ibexaShoppingList) { shoppingList.init(); /* Fetch user's shopping lists */ }
+    const shoppingList = new ShoppingList();
+    shoppingList.init(); // Fetch user's shopping lists
 
     const addToShoppingListsNodes = doc.querySelectorAll<HTMLDivElement>('.ibexa-sl-add-to-shopping-list');
     addToShoppingListsNodes.forEach((addToShoppingListNode) => {
