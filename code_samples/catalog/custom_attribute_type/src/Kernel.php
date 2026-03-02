4ce7b776557aa04d5fe2f3a44bdef38bc72e47cf
Mateusz Dębiński <mateusz.debinski@ibexa.co>
2026-03-02 16:06:32 +0100

    IBX-10685: Added information on how to add a custom attribute type to an existing storage definition

---
+++ code_samples/catalog/custom_attribute_type/src/Kernel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use App\DependencyInjection\AddFloatStorageDefinitionTag;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new AddFloatStorageDefinitionTag(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 10);
+    }
+}
