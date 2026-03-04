13dabf5e92242da5454f503264aec9bfa23e5e3a
adriendupuis <adriendupuis@users.noreply.github.com>
2026-03-04 14:43:26 +0000

    PHP & JS CS Fixes

--- code_samples/shopping_list/search/criteria.php
+++ code_samples/shopping_list/search/criteria.php
@@ -1,10 +1,9 @@
 <?php declare(strict_types=1);
 
-use Ibexa\Contracts\Core\Repository\PermissionResolver;
 use Ibexa\Contracts\ShoppingList\Value\Query;
 use Ibexa\Contracts\ShoppingList\Value\ShoppingListQuery;
 
-/** @var PermissionResolver $permissionResolver */
+/** @var \Ibexa\Contracts\Core\Repository\PermissionResolver $permissionResolver */
 $query = new ShoppingListQuery(
     new Query\Criterion\LogicalAnd(
         new Query\Criterion\OwnerCriterion($permissionResolver->getCurrentUserReference()),
