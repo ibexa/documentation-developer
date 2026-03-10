50498ddf8d891851399fb51bf418d6de167fde90
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2026-03-10 13:15:21 +0100

    Redesign share.html.twig for storefront

--- code_samples/collaboration/src/Form/Type/ShareCartType.php
+++ code_samples/collaboration/src/Form/Type/ShareCartType.php
@@ -5,6 +5,7 @@ namespace App\Form\Type;
 use App\Form\Data\ShareCartData;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,6 +18,8 @@ final class ShareCartType extends AbstractType
     {
         $builder->add('email', EmailType::class, [
             'label' => 'E-mail',
+        ])->add('submit', SubmitType::class, [
+            'label' => 'Share',
         ]);
     }
 
