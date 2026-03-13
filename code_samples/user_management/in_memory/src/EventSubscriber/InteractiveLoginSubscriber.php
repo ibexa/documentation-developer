922c4b6c506c8b32c654990d5662e1cafa8f1db3
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2026-03-13 13:56:50 +0100

    user_authentication.md: Move code to files

---
+++ code_samples/user_management/in_memory/src/EventSubscriber/InteractiveLoginSubscriber.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Ibexa\Contracts\Core\Repository\UserService;
+use Ibexa\Core\MVC\Symfony\Event\InteractiveLoginEvent;
+use Ibexa\Core\MVC\Symfony\MVCEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class InteractiveLoginSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var \Ibexa\Contracts\Core\Repository\UserService
+     */
+    private $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            MVCEvents::INTERACTIVE_LOGIN => 'onInteractiveLogin'
+        ];
+    }
+
+    public function onInteractiveLogin(InteractiveLoginEvent $event)
+    {
+        // This loads a generic User and assigns it back to the event.
+        // You may want to create Users here, or even load predefined Users depending on your own rules.
+        $event->setApiUser($this->userService->loadUserByLogin( 'lolautruche' ));
+    }
+}
