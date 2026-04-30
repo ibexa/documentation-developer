d78b477ec19e233c427ed7c1f056323f9715b74c
julitafalcondusza <julita.dusza@ibexa.co>
2026-04-30 13:40:51 +0200

    fixes in generic field type tutorial

--- code_samples/field_types/2dpoint_ft/src/FieldType/Point2D/Value_final.php
+++ code_samples/field_types/2dpoint_ft/src/FieldType/Point2D/Value_final.php
@@ -8,12 +8,17 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 final class Value implements ValueInterface
 {
-    public function __construct(
-        #[Assert\NotBlank]
-        private ?float $x = null,
-        #[Assert\NotBlank]
-        private ?float $y = null
-    ) {
+    #[Assert\NotBlank]
+    private ?float $x = null;
+    #[Assert\NotBlank]
+    private ?float $y = null;
+
+    public function __construct(array $coords = [])
+    {
+        if (!empty($coords)) {
+            $this->x = $coords[0];
+            $this->y = $coords[1];
+        }
     }
 
     public function getX(): ?float
