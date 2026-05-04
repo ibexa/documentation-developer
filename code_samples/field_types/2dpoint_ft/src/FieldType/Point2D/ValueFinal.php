d1b67588d7dc692f9eff6b7ad89db749418d2975
julitafalcondusza <julita.dusza@ibexa.co>
2026-05-04 10:56:35 +0200

    code fix

--- code_samples/field_types/2dpoint_ft/src/FieldType/Point2D/ValueFinal.php
+++ code_samples/field_types/2dpoint_ft/src/FieldType/Point2D/ValueFinal.php
@@ -6,7 +6,7 @@ namespace App\FieldType\Point2D;
 use Ibexa\Contracts\Core\FieldType\Value as ValueInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
-final readonly class ValueFinal implements ValueInterface
+final class ValueFinal implements ValueInterface
 {
     #[Assert\NotBlank]
     private ?float $x = null;
