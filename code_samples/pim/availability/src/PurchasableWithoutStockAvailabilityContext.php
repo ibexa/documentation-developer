dc929b2f49fcb53b935dc271eb2d3fed6ae7c2e2
Marek Nocoń <mnocon@users.noreply.github.com>
2026-05-22 11:27:01 +0200

    Expanded the product availability section (#3190)
    * Expanded product availability
    * Adapted to PHP 7,4
    
    
    ---------
    
    Co-authored-by: Tomasz Dąbrowski <64841871+dabrt@users.noreply.github.com>

---
+++ code_samples/pim/availability/src/PurchasableWithoutStockAvailabilityContext.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace App\ProductCatalog\Availability;
+
+use Ibexa\Contracts\ProductCatalog\Values\Availability\AvailabilityContextInterface;
+
+final class PurchasableWithoutStockAvailabilityContext implements AvailabilityContextInterface
+{
+}
