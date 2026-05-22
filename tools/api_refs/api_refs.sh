02712d31804e5f0b2f67b20ce2231bab0da16739
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2026-05-22 08:45:18 +0200

    PHP API Ref: keep images, rm favicons (#3231)

--- tools/api_refs/api_refs.sh
+++ tools/api_refs/api_refs.sh
@@ -191,8 +191,9 @@ fi;
 $PHP_BINARY $PHPDOC_BIN -t php_api_reference;
 if [ $? -eq 0 ]; then
   echo -n 'Remove unneeded from phpDocumentor output… ';
-  rm -rf ./php_api_reference/files ./php_api_reference/graphs ./php_api_reference/images ./php_api_reference/indices ./php_api_reference/packages;
-  rm -f ./php_api_reference/classes/Symfony-*.html ./php_api_reference/namespaces/symfony*.html
+  rm -rf ./php_api_reference/files ./php_api_reference/graphs ./php_api_reference/indices ./php_api_reference/packages;
+  rm -f ./php_api_reference/images/apple-touch-icon.png ./php_api_reference/images/favicon-16x16.png ./php_api_reference/images/favicon-32x32.png ./php_api_reference/images/favicon.ico;
+  rm -f ./php_api_reference/classes/Symfony-*.html ./php_api_reference/namespaces/symfony*.html;
   echo -n 'Remove Symfony namespace from index… ';
   awk 'NR==FNR{if (/.*"fqsen": "\\\\Symfony.*/) for (i=-1;i<=3;i++) del[NR+i]; next} !(FNR in del)' \
     ./php_api_reference/js/searchIndex.js \
