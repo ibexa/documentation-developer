34c233f48ac5f4c6cce932f2a3c16b69bd478070
Marek Nocoń <mnocon@users.noreply.github.com>
2026-07-31 15:39:35 +0200

    Improved error messages for failed YAML tests

--- tests/Yaml/YamlTest.php
+++ tests/Yaml/YamlTest.php
@@ -41,7 +41,7 @@ final class YamlTest extends TestCase
             }
 
             self::fail(sprintf(
-                'YAML parse error in %s at line %d [hash:%s]: %s',
+                'YAML parse error in %s:%d [hash:%s]: %s',
                 $filePath,
                 $line,
                 $bodyHash,
