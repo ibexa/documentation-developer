9e9d421fc864d46eb4b00a636b55228b84be7ba4
Tomasz Dąbrowski <64841871+dabrt@users.noreply.github.com>
2026-08-20 14:47:33 +0200

    IBX-10684: Document translation management (#3249)
    * IBX-10684: Document translation 
    
    ---------
    
    Co-authored-by: dabrt <dabrt@users.noreply.github.com>
    Co-authored-by: Marek Nocoń <mnocon@users.noreply.github.com>
    Co-authored-by: Tomasz Kryszan <tomasz.kryszan@ibexa.co>

---
+++ code_samples/translations_management/src/TranslationsManagement/MyCustomLanguageCodeNormalizer.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\TranslationsManagement;
+
+use Ibexa\Contracts\TranslationsManagement\AutoTranslate\Exception\UnsupportedLanguageException;
+use Ibexa\Contracts\TranslationsManagement\AutoTranslate\Provider\LanguageNormalizer\LanguageNormalizerInterface;
+use Ibexa\Contracts\TranslationsManagement\AutoTranslate\Provider\TranslationProviderInterface;
+
+final class MyCustomLanguageCodeNormalizer implements LanguageNormalizerInterface
+{
+    private const LANGUAGE_MAP = [
+        'eng-GB' => 'en-GB',
+        'ger-DE' => 'de',
+        'fre-FR' => 'fr',
+    ];
+
+    public function supports(TranslationProviderInterface $provider): bool
+    {
+        return $provider->getIdentifier() === 'my_custom_ai_provider';
+    }
+
+    public function normalize(
+        TranslationProviderInterface $provider,
+        string $languageCode
+    ): string {
+        if (isset(self::LANGUAGE_MAP[$languageCode])) {
+            return self::LANGUAGE_MAP[$languageCode];
+        }
+
+        throw new UnsupportedLanguageException(
+            $languageCode,
+            $provider->getIdentifier(),
+            array_values(self::LANGUAGE_MAP)
+        );
+    }
+}
