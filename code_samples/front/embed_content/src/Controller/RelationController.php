4d7378bee11feb763aec9f799a5f3be4f0fb7e7c
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2026-08-26 09:06:29 +0200

    5.0: Use RelationListIterator (#2548)
    To get all relations, use `RelationListIteratorAdapter` + `BatchIterator` instead of `ContentService::countRelations()` + `ContentService::loadRelationList()`
    
    ---------
    
    Co-authored-by: Marek Nocoń <mnocon@users.noreply.github.com>

--- code_samples/front/embed_content/src/Controller/RelationController.php
+++ code_samples/front/embed_content/src/Controller/RelationController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Ibexa\Contracts\Core\Repository\ContentService;
+use Ibexa\Contracts\Core\Repository\Iterator\BatchIterator;
+use Ibexa\Contracts\Core\Repository\Iterator\BatchIteratorAdapter\RelationListIteratorAdapter;
 use Ibexa\Contracts\Core\Repository\LocationService;
 use Ibexa\Core\MVC\Symfony\View\View;
 
@@ -21,11 +23,16 @@ class RelationController
         $location = $this->locationService->loadLocation($locationId);
         $contentInfo = $location->getContentInfo();
         $versionInfo = $this->contentService->loadVersionInfo($contentInfo);
-        $relationList = $this->contentService->loadRelationList($versionInfo);
+        $relationListIterator = new BatchIterator(
+            new RelationListIteratorAdapter(
+                $this->contentService,
+                $versionInfo
+            )
+        );
 
         $items = [];
 
-        foreach ($relationList as $relationListItem) {
+        foreach ($relationListIterator as $relationListItem) {
             if ($relationListItem->hasRelation() && in_array($relationListItem->getRelation()->getDestinationContentInfo()->getContentType()->identifier, $acceptedContentTypes)) {
                 $items[] = $this->contentService->loadContentByContentInfo($relationListItem->getRelation()->getDestinationContentInfo());
             }
